cd884ec34e3f51683e83268cd6866eca9894bafc
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2023-11-18 19:13:02 +0300

    add entities

---
+++ src/main/java/com/bezkoder/spring/security/postgresql/models/Image.java
@@ -0,0 +1,30 @@
+package com.bezkoder.spring.security.postgresql.models;
+
+import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String type;
+
+    @Lob
+    @Column(name = "image_data")
+    private byte[] imageData;
+}
