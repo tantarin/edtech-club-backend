99885cdd53e2b2f36f507ae9c821efdb0385d771
Tanya Tarinskaya <t.tarinskaya@itigris.ru>
2024-05-09 22:54:48 +0300

    правка

--- src/main/resources/db/migration/V3__add_data.sql
+++ src/main/resources/db/migration/V3__add_data.sql
@@ -1,6 +1,5 @@
 ALTER SEQUENCE public.ads_id_seq OWNED BY ads.id;
 ALTER SEQUENCE public.images_id_seq OWNED BY images.id;
 ALTER SEQUENCE public.news_id_seq OWNED BY news.id;
-ALTER SEQUENCE public.refreshtoken_id_seq OWNED BY refreshtoken.id;
 ALTER SEQUENCE public.roles_id_seq OWNED BY roles.id;
 ALTER SEQUENCE public.users_id_seq OWNED BY users.id;
